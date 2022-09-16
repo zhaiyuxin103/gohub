262b609b52b2fbef90c914dbce0a577cb8143fe5
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-09-16 19:26:12 +0800

    feat(config): 配置方案的实现

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
+
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	// new 一个 Gin Engine 实例
 	router := gin.New()
 
@@ -14,7 +29,7 @@ func main() {
 	bootstrap.SetupRoute(router)
 
 	// 运行服务，默认为 8080，这里指定端口为 8000
-	err := router.Run(":3000")
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理，端口被占用或者其他错误
 		fmt.Println(err.Error())
