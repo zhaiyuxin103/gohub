4ef8ed1afe447ce412f08490a49ae1a2f722a032
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-09-17 00:26:38 +0800

    feat(validate): 验证请求

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	v1 "gohub/app/http/controllers/api/v1"
 	"gohub/app/models/user"
+	"gohub/app/requests"
 	"net/http"
 )
 
@@ -17,11 +18,8 @@ type SignupController struct {
 // IsPhoneExist 检测手机号是否被注册
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 
-	// 请求对象
-	type PhoneExistRequest struct {
-		Phone string `json:"phone"`
-	}
-	request := PhoneExistRequest{}
+	// 初始化请求对象
+	request := requests.SignupPhoneExistRequest{}
 
 	// 解析 JSON 请求
 	if err := c.ShouldBindJSON(&request); err != nil {
@@ -32,6 +30,17 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 		// 打印错误请求
 		fmt.Println(err.Error())
 		// 出错了，中断请求
+		return
+	}
+
+	// 表单验证
+	errs := requests.ValidateSignupPhoneExist(&request, c)
+	// errs 返回长度等于 0 即通过，大于 0 即有错误发生
+	if len(errs) > 0 {
+		// 验证失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
 		return
 	}
 
