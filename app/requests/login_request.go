836d531bf0debceb230583c1397f7ea99097cf60
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-09-26 16:42:22 +0800

    feat(auth): 使用密码登录

--- app/requests/login_request.go
+++ app/requests/login_request.go
@@ -37,3 +37,47 @@ func LoginByPhone(data interface{}, c *gin.Context) map[string][]string {
 
 	return errs
 }
+
+type LoginByPasswordRequest struct {
+	CaptchaID     string `json:"captcha_id,omitempty" valid:"captcha_id"`
+	CaptchaAnswer string `json:"captcha_answer,omitempty" valid:"captcha_answer"`
+
+	LoginID  string `json:"login_id" valid:"login_id"`
+	Password string `json:"password,omitempty" valid:"password"`
+}
+
+// LoginByPassword 验证表单，返回长度等于零即通过
+func LoginByPassword(data interface{}, c *gin.Context) map[string][]string {
+
+	rules := govalidator.MapData{
+		"login_id":       []string{"required", "min:3"},
+		"password":       []string{"required", "min:6"},
+		"captcha_id":     []string{"required"},
+		"captcha_answer": []string{"required", "digits:6"},
+	}
+	messages := govalidator.MapData{
+		"login_id": []string{
+			"required:登录 ID 为必填项，支持手机号、邮箱和用户名",
+			"min:登录 ID 长度需大于 3",
+		},
+		"password": []string{
+			"required:密码为必填项",
+			"min:密码长度需大于 6",
+		},
+		"captcha_id": []string{
+			"required:图片验证码的 ID 为必填项",
+		},
+		"captcha_answer": []string{
+			"required:图片验证码答案为必填项",
+			"digits:图片验证码答案长度必须为 6 位的数字",
+		},
+	}
+
+	errs := validate(data, rules, messages)
+
+	// 图片验证码
+	_data := data.(*LoginByPasswordRequest)
+	errs = validators.ValidateCaptcha(_data.CaptchaID, _data.CaptchaAnswer, errs)
+
+	return errs
+}
