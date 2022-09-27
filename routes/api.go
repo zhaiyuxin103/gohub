5f465f4aee2d1692a1753125619af44ad7b1a768
翟宇鑫 <zhaiyuxin103@hotmail.com>
2022-09-27 11:03:45 +0800

    pref(middlewares): 限流中间件

--- routes/api.go
+++ routes/api.go
@@ -4,6 +4,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"gohub/app/http/controllers/api/v1/auth"
+	"gohub/app/http/middlewares"
 	"net/http"
 )
 
@@ -12,6 +13,12 @@ func RegisterAPIRoutes(r *gin.Engine) {
 
 	// 测试一个 v1 的路由组，所有 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
+
+	// 全局限流中间件：每小时限流。这里是所有 API（根据 IP）请求加起来。
+	// 作为参考 Github API 每小时最多 60 个请求（根据 IP）。
+	// 测试时，可以调高一点。
+	v1.Use(middlewares.LimitIP("200-H"))
+
 	{
 		// 注册一个路由
 		v1.GET("/", func(c *gin.Context) {
@@ -21,33 +28,38 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			})
 		})
 		authGroup := v1.Group("/auth")
+		// 限流中间件：每小时限流，作为参考 Github API 每小时最多 60 个请求（根据 IP）
+		// 测试时，可以调高一点
+		authGroup.Use(middlewares.LimitIP("1000-H"))
 		{
+			// 注册用户
 			suc := new(auth.SignupController)
 			// 判断手机号是否已被注册
-			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+			authGroup.POST("/signup/phone/exist", middlewares.GuestJWT(), middlewares.LimitPerRoute("60-H"), suc.IsPhoneExist)
 			// 判断 Email 是否已被注册
-			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
-			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
-			authGroup.POST("/signup/using-email", suc.SignupUsingEmail)
+			authGroup.POST("/signup/email/exist", middlewares.GuestJWT(), middlewares.LimitPerRoute("60-H"), suc.IsEmailExist)
+			authGroup.POST("/signup/using-phone", middlewares.GuestJWT(), suc.SignupUsingPhone)
+			authGroup.POST("/signup/using-email", middlewares.GuestJWT(), suc.SignupUsingEmail)
 
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
 			// 图片验证码，需要加限流
-			authGroup.POST("/verify-codes/captcha", vcc.ShowCaptcha)
-			authGroup.POST("/verify-codes/phone", vcc.SendUsingPhone)
-			authGroup.POST("/verify-codes/email", vcc.SendUsingEmail)
+			authGroup.POST("/verify-codes/captcha", middlewares.LimitPerRoute("50-H"), vcc.ShowCaptcha)
+			authGroup.POST("/verify-codes/phone", middlewares.LimitPerRoute("20-H"), vcc.SendUsingPhone)
+			authGroup.POST("/verify-codes/email", middlewares.LimitPerRoute("20-H"), vcc.SendUsingEmail)
 
+			// 登录
 			lgc := new(auth.LoginController)
 			// 使用手机号 + 短信验证码进行登录
-			authGroup.POST("/login/using-phone", lgc.LoginByPhone)
+			authGroup.POST("/login/using-phone", middlewares.GuestJWT(), lgc.LoginByPhone)
 			// 支持手机号，Email 和用户名
-			authGroup.POST("/login/using-password", lgc.LoginByPassword)
-			authGroup.POST("/login/refresh-token", lgc.RefreshToken)
+			authGroup.POST("/login/using-password", middlewares.GuestJWT(), lgc.LoginByPassword)
+			authGroup.POST("/login/refresh-token", middlewares.AuthJWT(), lgc.RefreshToken)
 
 			// 重置密码
 			pwc := new(auth.PasswordController)
-			authGroup.POST("/password-reset/using-phone", pwc.ResetByPhone)
-			authGroup.POST("/password-reset/using-email", pwc.ResetByEmail)
+			authGroup.POST("/password-reset/using-phone", middlewares.GuestJWT(), pwc.ResetByPhone)
+			authGroup.POST("/password-reset/using-email", middlewares.GuestJWT(), pwc.ResetByEmail)
 		}
 	}
 }
